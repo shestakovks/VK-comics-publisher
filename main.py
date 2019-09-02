0fc7a5ac6cca1d35ce4465db98832b6fa0107bbf
Shestakov Konstantin <shestakov.ks94@gmail.com>
2019-09-02 10:45:32 +0300

    small refactoring

--- main.py
+++ main.py
@@ -10,18 +10,27 @@ def get_filename_from_url(url):
     return url.split('/')[-1]
 
 
-def load_posted(filename=None):
-    if filename is None:
-        filename = 'posted.json'
+def check_error_in_response(response):
+    """Checks if answer is 200 but still contains error.
+
+    Arguments:
+        response {dict} -- result of requests.METHOD().json().
+
+    Raises:
+        requests.exceptions.HTTPError: Raises if error in answer with 200.
+    """
+    if 'error' in response:
+        raise requests.exceptions.HTTPError(response['error'])
+
+
+def load_posted(filename='posted.json'):
     if not os.path.isfile(filename):
         return {}
     with open(filename) as f:
         return json.load(f)
 
 
-def save_posted(posted, filename=None):
-    if filename is None:
-        filename = 'posted.json'
+def save_posted(posted, filename='posted.json'):
     with open(filename, 'w') as f:
         json.dump(posted, f, indent=2)
 
@@ -44,24 +53,25 @@ def get_comics(comics_number):
     comics_url = f'https://xkcd.com/{comics_number}/info.0.json'
     comics = requests.get(comics_url)
     comics.raise_for_status()
+    comics_json = comics.json()
 
-    comics_pic_url = comics.json()['img']
+    comics_pic_url = comics_json['img']
     filename = get_filename_from_url(comics_pic_url)
     download_photo(comics_pic_url, filename)
 
-    comics_comment = comics.json()['alt']
+    comics_comment = comics_json['alt']
     return filename, comics_comment
 
 
 def get_random_comics():
-    """Generates random comics number and downloads it to working dir
+    """Generates random comics number and downloads it to working dir.
 
     Returns:
         (filename, comment) -- filename of downloaded comics picture
-        and witty author's comment about it
+        and witty author's comment about it.
 
     Raises:
-        requests.exceptions.HTTPError if anything went wrong
+        requests.exceptions.HTTPError if anything went wrong.
     """
     max_comics_number = get_max_comics_num()
     is_posted = True
@@ -75,8 +85,7 @@ def get_random_comics():
 def get_wall_upload_url(url, params):
     response = requests.get(url.format('photos.getWallUploadServer'),
                             params=params).json()
-    if 'error' in response:
-        raise requests.exceptions.HTTPError(response['error'])
+    check_error_in_response(response)
     return response['response']['upload_url']
 
 
@@ -86,16 +95,14 @@ def upload_pic_to_server(url, filename):
             'photo': pic,
         }
         response = requests.post(url, files=files).json()
-    if 'error' in response:
-        raise requests.exceptions.HTTPError(response['error'])
+    check_error_in_response(response)
     return response
 
 
 def save_wall_photo(url, params):
     response = requests.post(url.format('photos.saveWallPhoto'),
                              data=params).json()
-    if 'error' in response:
-        raise requests.exceptions.HTTPError(response['error'])
+    check_error_in_response(response)
     return response
 
 
@@ -111,23 +118,22 @@ def post_to_wall(url, base_params, post_params, comment):
         'message': comment,
     }
     response = requests.post(url.format('wall.post'), data=params).json()
-    if 'error' in response:
-        raise requests.exceptions.HTTPError(response['error'])
+    check_error_in_response(response)
 
 
 def post_comics_to_vk_group(filename, comment, vk_token,
                             vk_api_version, vk_group_id):
-    """Post given picture and comment to given VK group wall
+    """Post given picture and comment to given VK group wall.
 
     Arguments:
-        filename {str} -- path to file with picture
-        comment {str} -- message to add to posted picture
-        vk_token {str} -- your VK token
-        vk_api_version {str} -- VK API version you'd like to use
-        vk_group_id {str} -- your VK group id
+        filename {str} -- path to file with picture,
+        comment {str} -- message to add to posted picture,
+        vk_token {str} -- your VK token,
+        vk_api_version {str} -- VK API version you'd like to use,
+        vk_group_id {str} -- your VK group id.
 
     Raises:
-        requests.exceptions.HTTPError if anything went wrong
+        requests.exceptions.HTTPError if anything went wrong.
     """
     vk_url = 'https://api.vk.com/method/{}'
     base_params = {
